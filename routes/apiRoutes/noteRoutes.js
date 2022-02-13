20c946e479d2d8c8790fd00bb486e8cb7e2139fa
zanescheel-fTEJAV <zane.scheel@gmail.com>
2022-02-13 03:09:32 -0600

    added a GET route

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const notes = require('../../db/db.json');
 
+router.get('/notes', (req, res) => {
+    const results = notes;
+
+    res.json(notes);
+})
+
+router.post('/api/notes', (req, res) => {
+
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoutes');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 
